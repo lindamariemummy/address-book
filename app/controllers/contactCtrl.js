f0fba1262c8e58a452b6ae94d3161c8b4c829037
lindamariemummy <lindamariemummy@gmail.com>
2014-12-08 16:42:21 -0800

    fix jscs error

--- app/controllers/contactCtrl.js
+++ app/controllers/contactCtrl.js
@@ -5,7 +5,8 @@ module.exports = function(app) {
 
     $scope.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
                        'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-                       'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+                       'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+                       'Y', 'Z'];
 
     //add a contact
     $scope.saveContact = function() {
